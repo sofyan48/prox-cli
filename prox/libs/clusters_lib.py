9b70448f1ad426cd313f198fbcd320b3199aa560
Sofyan Saputra <meongbego@gmail.com>
2019-01-20 18:59:37 +0700

    Add: cluster Node List Example

---
+++ prox/libs/clusters_lib.py
@@ -0,0 +1,17 @@
+from prox.libs import login_lib
+
+
+def get_auth():
+    try:
+        prox = login_lib.load_dumped_session()
+    except Exception as e:
+        print(e)
+        exit()
+    else:
+        return prox
+
+def list_cluster():
+    prox = get_auth()
+    a = prox.getClusterStatus()
+    print(a)
+
