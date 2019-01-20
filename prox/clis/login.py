dceec99e8e7a56cbc590d7c9b1e0179e32417321
Sofyan Saputra <meongbego@gmail.com>
2019-01-20 18:58:02 +0700

    Add: fix Login Connect API Proxmox

--- prox/clis/login.py
+++ prox/clis/login.py
@@ -1,27 +1,48 @@
 from prox.clis.base import Base
-from prox.libs import login
 from getpass import getpass
+from prox.libs import login_lib
 import os
 
+APP_HOME = login_lib.utils.APP_HOME
 
-CURR_DIR = os.getcwd()
-
-class Login(Base):
+class Login(Base): 
     """
         usage:
             login
-            login neo
-            login docker
 
-        Build Project
+        Commands :
+            login                         Build Yaml File
 
         Options:
         -h --help                             Print usage
     """
-
     def execute(self):
-        if self.args['neo']:
+        env = None
+        if os.path.exists(APP_HOME+"/.prox.env"):
+            print("Environment Exists Do You remove :")
+            checks = login_lib.utils.question("Choose Y/N ")
+            if checks == 'Y' or checks == 'y':
+                username = input("Username: ")
+                password = getpass("Password: ")
+                auth_url = input("Host: ")
+                os.remove(APP_HOME+"/.prox.env")
+                login_lib.create_env_file(username, password, auth_url)
+            else:
+                env = login_lib.utils.get_env_values()
+        else:
             username = input("Username: ")
             password = getpass("Password: ")
-            login.login(username , password)
+            auth_url = input("Host: ")
+            login_lib.create_env_file(username, password, auth_url)
+            env = login_lib.utils.get_env_values()
 
+
+        prox = login_lib.connect_proxmox(env['project_url'], env['username'], env['password'])
+
+        # check login
+        if login_lib.utils.read_file("/tmp/prox.pkl"):
+            os.remove("/tmp/prox.pkl")
+        login_lib.dump_session(prox)
+        if not login_lib.check_session():
+            login_lib.utils.log_err("Login Not Success")
+        login_lib.utils.log_info("Login Success")
--- prox/libs/login_lib.py
+++ prox/libs/login_lib.py
@@ -1,4 +1,4 @@
-from passlib.hash import pbkdf2_sha256
+# from passlib.hash import pbkdf2_sha256
 from prox.libs import utils
 from prox.libs import proxmox
 import os
@@ -22,7 +22,7 @@ def create_env_file(username, password, auth_url = None, port = None):
 
 def dump_session(sess):
     try:
-        with open('/tmp/session.pkl', 'wb') as f:
+        with open('/tmp/prox.pkl', 'wb') as f:
             dill.dump(sess, f)
     except Exception:
         utils.log_err("Dump session failed")
@@ -32,7 +32,7 @@ def load_dumped_session():
     try:
         if check_session():
             sess = None
-            with open('/tmp/session.pkl', 'rb') as f:
+            with open('/tmp/prox.pkl', 'rb') as f:
                 sess = dill.load(f)
             return sess
         else:
@@ -44,24 +44,7 @@ def load_dumped_session():
 
 
 def check_session():
-    return os.path.isfile("/tmp/session.pkl")
-
-
-def login(username, password, auth_url):
-    password = pbkdf2_sha256.hash(password)
-    if os.path.exists(APP_HOME+"/.prox.env"):
-        print("Environment Exists Do You remove :")
-        checks = input("Y/N")
-        if checks == 'Y' or checks == 'y':
-            os.remove(APP_HOME+"/.prox.env")
-            create_env_file(username, password, auth_url)
-        else:
-            env = utils.get_env_values()
-            print(env['username'])
-
-    create_env_file(username, password, auth_url)
-
-    
+    return os.path.isfile("/tmp/prox.pkl")
 
 
 def logout():
@@ -69,6 +52,10 @@ def logout():
         os.remove(APP_HOME+"/.prox.env")
     else:
         print("Not Current Sessions")
+
+def connect_proxmox(host, username, password):
+    prox_at = proxmox.proxmox_auth(host, username, password)
+    return prox_at
 
 
 
