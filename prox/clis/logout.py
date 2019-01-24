651d5e612a30d5c5ce78fe9efab6b5cf0121bcf0
Sofyan Saputra <meongbego@gmail.com>
2019-01-24 16:29:56 +0700

    Add: Logout Command

---
+++ prox/clis/logout.py
@@ -0,0 +1,20 @@
+from prox.clis.base import Base
+from getpass import getpass
+from prox.libs import login_lib
+import os
+
+APP_HOME = login_lib.utils.APP_HOME
+
+class Logout(Base): 
+    """
+        usage:
+            login
+
+        Commands :
+            login                         Build Yaml File
+
+        Options:
+        -h --help                             Print usage
+    """
+    def execute(self):
+        login_lib.logout()
