6bf4e1950e218cc295f0d6dcdd037f682ed20ebd
Sofyan Saputra <meongbego@gmail.com>
2019-01-26 05:22:34 +0700

    Edit: CL Lib Session Models

--- prox/libs/clusters_lib.py
+++ prox/libs/clusters_lib.py
@@ -1,25 +1,30 @@
 from prox.libs import login_lib
 
 
-def get_auth(session = None):
+def get_auth(session=None):
     if not session:
-        prox = login_lib.load_dumped_session()
+        try:
+            prox = login_lib.load_dumped_session()
+        except Exception as e:
+            print(e)
+            exit()
+        else:
+            return prox
     else:
         return session
-    return prox
 
 def list_cluster():
     prox = get_auth()
     cl_list = prox.getClusterStatus()
     return cl_list
 
-def cluster_service(node):
-    prox = get_auth()
+def cluster_service(node, session=None):
+    prox = get_auth(session)
     cluster_service = prox.getNodeServiceList(node)
     return cluster_service['data']
 
-def service_detail(node, service):
-    prox = get_auth()
+def service_detail(node, service, session=None):
+    prox = get_auth(session)
     detail_service = prox.getNodeServiceState(node, service)
     return detail_service['data']
 
